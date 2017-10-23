9d7c3d994b1f8d8c1530845155dadc0cfe288a13
AzureAD\RakibuzzamanMahmd <i-Rakibuzzaman.Mahmd@ekkdam.com>
2017-10-23 16:21:13 +0600

    mergesort

--- Mergesort/merge_sort_practice.py
+++ Mergesort/merge_sort_practice.py
@@ -2,30 +2,11 @@ class MergeSort():
     def __init__(self,numbers):
         self.values = numbers
         self.count = len(numbers)
-    
+  
     def sort(self):
-        self.merge_sort(0,self.count - 1)
-        return self.values
-
 
     def merge_sort(self,low,high):
-        if low<high:
-            mid = (low+high)/2
-            print(mid)
-
-            self.merge_sort(low,mid)
-            self.merge_sort(mid+1,high)
-            #self.merge(low,mid,high)
 
 
     def merge(self,low,mid,high):
-        b = []
-        i = low
-        j = mid + 1
-
-        while i <= mid and j<=high:
-            
-
-
-
 
