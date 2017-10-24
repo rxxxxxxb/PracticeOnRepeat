c5039f7bb2f9f7b03c7f904fe4bb698a4e96a72a
AzureAD\RakibuzzamanMahmd <i-Rakibuzzaman.Mahmd@ekkdam.com>
2017-10-24 11:27:04 +0600

    merge practiced

---
+++ Mergesort/main_practice.py
@@ -0,0 +1,20 @@
+from merge_sort_practice import MergeSort
+
+
+
+def main():
+    num = [325432, 989, 547510, 3, -93, 189019, 5042, 123,
+                597, 42, 7506, 184, 184, 2409, 45, 824]
+
+    #num = original[:]
+
+    ms = MergeSort(num)
+
+    output = ms.sort()
+
+
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
