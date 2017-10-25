f2893aab94cd751988fe94d9ef385a642367dd76
rkib <rkib>
2017-10-25 22:09:42 +0600

    practiced

--- Mergesort/merge_sort_practice.py
+++ Mergesort/merge_sort_practice.py
@@ -1,9 +1,25 @@
 class MergeSort():
     def __init__(self,numbers):
+        self.values = numbers
+        self.count = len(numbers)
+
 
     def sort(self):
+        self.merge_sort(0, self.count -1)
+        return self.values
 
     def merge_sort(self,low,high):
+       if low < high:
+           mid = (low +high)//2
 
+           self.merge_sort(low,mid)
+           self.merge_sort(mid +1 ,high)
+           self.merge(low,mid,high)
 
-    def merge(self,low,mid,high):            
+    def merge(self,low,mid,high):
+        b = []
+        i = low
+        j = mid + 1
+
+        while i <= mid and j <= high:
+            if self.values[i] <= self.values[j]:
