4ee3b203a0d53bec2948a85aa7bd1014e872d088
rkib <rkib>
2017-10-29 00:05:21 +0600

    Practiced

--- Mergesort/merge_sort_practice.py
+++ Mergesort/merge_sort_practice.py
@@ -1,48 +1,47 @@
 class MergeSort():
-    def __init__(self,numbers):
-        self.values = numbers
-        self.count = len(numbers)
-
+    def __init__(self,number):
+        self.values = number
+        self.count = len(number)
 
     def sort(self):
-        self.merge_sort(0, self.count -1)
+        self.merge_sort(0 ,self.count - 1)
         return self.values
 
     def merge_sort(self,low,high):
-       if low < high :
-           mid = (low + high) // 2
-           
-           self.merge_sort(low,mid)
-           self.merge_sort(mid +1, high)
-           self.merge(low,mid,high)
+        if low < high:
+
+            mid = (high + low) // 2
+
+            self.merge_sort(low,mid)
+            self.merge_sort(mid + 1, high)
+            self.merge(low,mid,high)
 
     def merge(self,low,mid,high):
-        v = []
-        i= low
-        j= mid + 1
+        a = []
+        i = low
+        j = mid + 1
 
-        while i<= mid and j <=high:
-            if self.values[i]< self.values[j]:
-                v.append(self.values[i])
+        while i <= mid and j <= high:
+            if self.values[i] <= self.values[j]:
+                a.append(self.values[i])
                 i += 1
             else:
-                v.append(self.values[j])
-                j += 1    
+                a.append(self.values[j])
+                j += 1
 
-        while i <= mid:
-            v.append(self.values[i])
+        while i<=mid:
+            a.append(self.values[i])
             i += 1
 
-        while j <= high:
-            v.append(self.values[j])
-            j += 1    
+        while j<= high:
+            a.append(self.values[j])
+            j += 1     
 
-        for index, val in enumerate(v):
-            self.values[low + index] = val           
+        for index, v in enumerate(a):
+            self.values[low+index] = v            
 
 
-      
+        
 
-    
-    
+          
 
