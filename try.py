63288bf2509fabc1670ac91d7204c70813dcfa82
rkib <rkib>
2017-10-30 00:10:26 +0600

    tried animals function

---
+++ try.py
@@ -0,0 +1,14 @@
+# animals = ['cat','dog','bird']
+# for index, value in enumerate(animals):
+#     print (index, value)
+
+
+animals = ['cat','dog','bird']
+# l = len(animals)
+# for index in range(l):
+#     print (index, animals[index])   
+
+
+f=animals[0]
+
+print(f)
