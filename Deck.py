49930f2c74d1f4c821478d316f523b1ffb9a8342
AzureAD\RakibuzzamanMahmd <i-Rakibuzzaman.Mahmd@ekkdam.com>
2017-11-05 14:01:03 +0600

    finished deck of card

--- Deck.py
+++ Deck.py
@@ -1,34 +1,67 @@
+import random
+
 class Card(object):
     def __init__(self,suit,val):
         self.suit = suit
         self.value = val
 
     def show(self):
-        print ("{} of {}".format( self.suit,self.value)
+        print ("{} of {}".format( self.suit,self.value))
    
-
-     def show(self):
-         print ("{} of {}".format( self.suit,self.value)
 
 class Deck(object):
     def __init__(self):
-        self.card = []
+        self.cards = []
         self.build()
 
     def build(self):
         for s in ["Spades","Clubs","Diamonds","Hearts"]:
             for v in range(1, 14):
-                #self.cards.append(Card(s,v))
-                print ("{} of {}".format(v,s))
+                self.cards.append(Card(s,v))
+                #print ("{} of {}".format(v,s))
 
-    # def show(self):
-    #     for c in self.cards:
-    #         c.show()
+    def show(self):
+        for c in self.cards:
+            c.show()
 
-# class Player():
-#     def __init__()
 
-# card = Card("Clubs",6)
+    def shuffle(self):
+        for i in range(len(self.cards)-1,0,-1):
+            #print(i)
+            r = random.randint(0,i)
+            self.cards[i],self.cards[r] = self.cards[r],self.cards[i]  
 
-# card.show()
+    def drawCard(self):
+        return self.cards.pop()          
+
+class Player():
+    def __init__(self,name):
+        self.name = name
+        self.hand = []
+
+    def draw(self,deck):
+        self.hand.append(deck.drawCard()) 
+        return self  
+
+    def showHand(self):
+        for card in self.hand:
+            card.show()  
+
+    def discard(self):
+        return self.hand.pop()           
+
+
+
 deck = Deck()
+deck.shuffle()
+#card = deck.draw()
+#card.show()
+
+lol = Player("lol")
+
+for num in range(5):
+    lol.draw(deck)
+    
+#lol.draw(deck).draw(deck)
+lol.showHand()
+# deck.show()
---
+++ Python the hard way/exercise2.py
@@ -0,0 +1,10 @@
+print("lol %s " % 'hahah')
+
+a = 'A'
+b = 'B'
+c = 'C'
+d = 'Dddd'
+
+print("k"* 10)
+
+print(a+b+c+d,end='' )
