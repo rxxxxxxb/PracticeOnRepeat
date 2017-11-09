b9ccf266d64f61070622df7195f27e6705cf5e20
AzureAD\RakibuzzamanMahmd <i-Rakibuzzaman.Mahmd@ekkdam.com>
2017-11-09 12:41:04 +0600

    deck

--- Deck of card/Deck_practice.py
+++ Deck of card/Deck_practice.py
@@ -1,67 +1,49 @@
+
 import random
 
 class Card():
-    def __init__(self,number,suits):
-        self.suits = suits
-        self.number = number
+    def __init__(self,card,value):
+        self.card = card
+        self.value = value
 
-    def print(self):
-        print("{} of {}".format(self.number,self.suits))  
+    def show(self):
+        print("{} of {}".format(self.value,self.card))
 
-class Deck():
+class Deck:
     def __init__(self):
         self.cards = []
         self.build()
 
-    def build(self):
-        for suits in ['spades','hearts','diamonds','clubs']:
-            for value in range(1,14):
-                self.cards.append(Card(suits,value))
-   
-        #52 card build
 
-    def show(self):
-        for val in self.cards:
-            val.print()
-        #show all cards 
+    def build(self):
+        for card in  ['Clubs',"Spades","Hearts",'Diamonds']:
+            for val in range(1,14):
+                self.cards.append(Card(card,val))
+
 
     def shuffle(self):
-        l = len(self.cards)
-        for i in range(l):
-            r = random.randint(0,i)
-            self.cards[i],self.cards[r] = self.cards[r],self.cards[i]
-               
-        #shuffle cards
+        l = len((self.cards))
+        for le in range(l):
+            r = random.randint(0,le)
+            self.cards[le],self.cards[r] = self.cards[r],self.cards[le]
+
+
+
+    def show(self):
+        for v in self.cards:
+            v.show()            
 
 
     def drawCard(self):
-        return self.cards.pop()
-        #pop last card
+        return self.cards.pop()    
 
 
-class Player():
-    def __init__(self,name):
-        self.name = name
-        self.hand = []
+# clubs = Card("clubs","8")
 
-    def draw(self):
-        for card in range(5):
-            self.hand.append(deck.drawCard())
+# clubs.show()
 
-
-    def showHand(self):
-        for i in self.hand:
-            i.print()
-
-
-
-
-deck = Deck()  
+deck = Deck()
 deck.shuffle()
-
-pl1 = Player("pl1")
-
-pl1.draw()   
-#deck.shuffle()   
-##deck.show()
-pl1.showHand()
+#deck.show()
+deck.drawCard()
+deck.show()
