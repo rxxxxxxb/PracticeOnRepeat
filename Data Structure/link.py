630539a60d8e05a85ab1c115005cac8498992592
AzureAD\RakibuzzamanMahmd <i-Rakibuzzaman.Mahmd@ekkdam.com>
2017-12-03 15:00:48 +0600

    ll to arry

--- Data Structure/link.py
+++ Data Structure/link.py
@@ -14,20 +14,53 @@ class Stack:
         self.head = self.Node(e,self.head)
         self.size += 1
 
+    def isempty(self):
+        return self.size == 0    
+
     def top(self):
+        if self.isempty():
+            raise Exception('Empty')
         return self.head.element
 
     def length(self):
         return self.size
 
+    def pop(self):
+        pop = self.head.element
+        self.head = self.head.next
+        self.size =-1
+        return pop
+
 stack = Stack()         
 
-for r in  range(100):
-    rand = random.randint(0,r)
+for r in  range(10):
+    rand = random.randint(0,100)
     stack.push(rand)
 #stack.push(500)
+ 
+#stack.pop()
+l = stack.length()
+arry = []
 
-len = stack.length()
-top = stack.top()
-print(top)
-print(len)
+
+
+
+for ran in range(l):# converting linked list to array
+    temp = stack.pop()
+    print("arr : " ,temp)
+    arry.append(temp)
+    print("size " , len(arry))
+
+
+
+for  r, v  in enumerate(arry):
+    print(r,v)
+
+
+#     #stack.pop()
+#     print(r,v)
+#     len = stack.length()
+#     top = stack.top()
+#     print(top)
+#     print(len)
+#     stack.pop()
