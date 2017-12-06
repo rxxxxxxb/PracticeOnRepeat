28a9850de8aec922e6e35840ef5b7e2b05a9895d
rkib <rkib>
2017-12-07 01:00:50 +0600

    sapiens

--- profile_practice.py
+++ profile_practice.py
@@ -11,6 +11,8 @@ def user(first,last,**info):
 
 dawking = user("Richard","Dawking",Book ="God Delusion") 
 harris = user("Sam","Harris",Book = "Waking Up")
+Yuval = user("Yoval","Noah Harrari", book = "Sapiens, Homo Deus")
 
 print(dawking)   
 print(harris)
+print(Yuval)
