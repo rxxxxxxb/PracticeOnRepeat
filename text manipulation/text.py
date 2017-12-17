83d6398bec51f0e563eddd457dbad9a681a3007e
rkib <rkib>
2017-12-18 00:47:04 +0600

    text

---
+++ text manipulation/text.py
@@ -0,0 +1,34 @@
+import string
+
+
+def load_words():
+    words = {}
+    exclude = set(string.punctuation + "\n")
+    word_count = 0
+
+    with open("tale-of-two-cities.txt", "r") as content:
+        for line in content:
+            line = line.replace("--", ' ')
+            line_words = line.split(" ")
+            for word in line_words:
+                word = ''.join(ch for ch in word if ch not in exclude)
+                print(word)
+
+
+                # add to dict and keep track of times it occurs
+                if word:
+                    word = word.lower()
+                    word_count += 1
+                    if word in words:
+                        words[word] += 1
+                    else:
+                        words[word] = 1
+
+    return words
+
+def main():
+    words = load_words()
+    word_count = len(words)
+    print(word_count)
+
+main()    
