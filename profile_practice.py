f21b6ddaa32248dc5ba33c2b1dbd24a8d382d14c
rkib <rkib>
2017-12-22 00:49:12 +0600

    writers

--- profile_practice.py
+++ profile_practice.py
@@ -1,18 +1,28 @@
 def user(first,last,**info):
     profile = {}
 
-    profile["FIrst name"] = first
-    profile["Last name "] = last
+    profile["First name"] = first
+    profile["Last name"] = last
 
     for key,range in info.items():
         profile[key] = range
 
     return profile
 
-dawking = user("Richard","Dawking",Book ="God Delusion") 
-harris = user("Sam","Harris",Book = "Waking Up")
-Yuval = user("Yoval","Noah Harrari", book = "Sapiens, Homo Deus")
+dawking = user("Richard","Dawking",book ="God Delusion") 
+harris = user("Sam","Harris",book = "Waking Up")
+yuval = user("Yuval","Noah Harrari", book = "Sapiens, Homo Deus")
 
 print(dawking)   
 print(harris)
-print(Yuval)
+print(yuval)
+
+writersInfo = [dawking,harris,yuval]
+
+# Books = [book["book"] for book in writersInfo]
+# print("All the book" ,Books)
+
+writers = [writer["First name" and "Last name"] for writer in writersInfo]
+print(writers)
+
+#students = [student["mark"] for student in collection.find({}) if student["mark"] == 900]
